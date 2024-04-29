318f27bf933705856855b5d020978f6e8e8ce12f
VAbrishamii <vahide.abri@yahoo.com>
2024-04-29 15:11:59 +0200

    Commit

--- js/navbar.js
+++ js/navbar.js
@@ -5,32 +5,55 @@ const logo = document.createElement("div");
 logo.innerHTML = "<img src='asset/images/logo.png' alt='Trip' class='logo'>"; 
 mainContent.appendChild(logo);
 
-
+// Create the menu
 const navDiv = document.createElement("nav");
 const navbarDiv = document.createElement("div");
 navbarDiv.classList.add("navbar");
-const ul = document.createElement("ul");
-ul.classList.add("menu");
+const ulMenu = document.createElement("ul");
+ulMenu.classList.add("menu");
 ["Home", "About", "Contact Us"].forEach((text) => {
   const li = document.createElement("li");
   li.classList.add("menu-item");
   li.textContent = text;
-  ul.appendChild(li);
+  ulMenu.appendChild(li);
 });
-navbarDiv.appendChild(ul);
-mainContent.appendChild(navbarDiv);
+navbarDiv.appendChild(ulMenu);
+navDiv.appendChild(navbarDiv);
 
-const iconDiv = document.createElement("div");
-iconDiv.classList.add("icon");
-const magnifyingGlassIcon = document.createElement("i");
-magnifyingGlassIcon.classList.add("fa-solid", "fa-magnifying-glass");
-const userIcon = document.createElement("i");
-userIcon.classList.add("fa-regular", "fa-user");
-iconDiv.appendChild(magnifyingGlassIcon);
-iconDiv.appendChild(userIcon);
-navDiv.appendChild(iconDiv);
+// Create the icons
+const iconDiv = document.createElement('div');
+iconDiv.classList.add('icon');
+
+const iconList = document.createElement('ul');
+iconList.classList.add('icon-item');
+
+const magnifyingGlassIcon = document.createElement('li');
+const magnifyingGlassIconContent = document.createElement('i');
+magnifyingGlassIconContent.classList.add('fa-solid', 'fa-magnifying-glass');
+magnifyingGlassIcon.appendChild(magnifyingGlassIconContent);
+
+const userIcon = document.createElement('li');
+const userIconContent = document.createElement('i');
+userIconContent.classList.add('fa-solid', 'fa-user');
+userIcon.appendChild(userIconContent);
+
+const barsIcon = document.createElement('li');
+const barsIconContent = document.createElement('i');
+barsIconContent.classList.add('fa-solid', 'fa-bars', 'burger');
+barsIcon.appendChild(barsIconContent);
+
+// Append icons to each other and to the document
+magnifyingGlassIcon.appendChild(magnifyingGlassIconContent);
+userIcon.appendChild(userIconContent);
+barsIcon.appendChild(barsIconContent);
+
+iconList.appendChild(magnifyingGlassIcon);
+iconList.appendChild(userIcon);
+iconList.appendChild(barsIcon);
+iconDiv.appendChild(iconList);
 
 mainContent.appendChild(navDiv);
+mainContent.appendChild(iconDiv);
 
 const container = document.getElementById("container");
 container.appendChild(mainContent);
